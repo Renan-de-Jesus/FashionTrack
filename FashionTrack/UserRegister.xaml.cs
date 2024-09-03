db55e8471808a9c9767c056647b92f39501588f6
unknown <guilhermecella8@gmail.com>
2024-09-02 21:51:14 -0300

    solved the logic in the register screen

--- FashionTrack/UserRegister.xaml.cs
+++ FashionTrack/UserRegister.xaml.cs
@@ -30,8 +30,52 @@ namespace FashionTrack
 
         }
 
-        private void btnCadastrar_Click(object sender, RoutedEventArgs e)
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
+            string name = txtBoxName.Text;
+            string username = txtBoxUser.Text;
+            string password = pwdPassword.Password;
+            string confirmPassword = pwdConfirmPassword.Password;
+            bool isAdmin = toggleAdmin.IsChecked == true;
+
+            string connectionString = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+                try
+                {
+                    connection.Open();
+                    string querry = "INSERT INTO Usuarios(NomeCompleto, Usuario, Senha, Adm) VALUES (@name, @username, @password, @IsAdmin)";
+                    SqlCommand command = new SqlCommand(querry, connection);
+
+                    command.Parameters.AddWithValue("@name", name);
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@Password", password);
+                    command.Parameters.AddWithValue("@IsAdmin", isAdmin);
+
+                    if (password == confirmPassword)
+                    {
+                        int result = command.ExecuteNonQuery();
+
+                        if ((result > 0))
+                        {
+
+                            MessageBox.Show("Usuário cadastrado com sucesso!", "Sucesso", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Falha ao cadastrar o usuário.", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("As senhas não coincidem.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show($"Erro de SQL: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
         }
     }
 }
