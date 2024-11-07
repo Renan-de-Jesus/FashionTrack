e9e6012f4b37b841cf3b74299639d991b5e91944
ㅤLast <guilhermecella8@gmail.com>
2024-11-07 20:59:33 -0300

    Finished the desing in the customerScreen

--- SQLPrincipal.sql
+++ SQLPrincipal.sql
@@ -1,101 +1,166 @@
-CREATE DATABASE FashionTrack
+CREATE DATABASE FASHIONTRACK
 GO
 
-USE FashionTrack
+USE FASHIONTRACK
 GO
 
-CREATE TABLE Usuarios (
-    ID_Usuario INT IDENTITY(1,1),
-    NomeCompleto VARCHAR(100) NOT NULL,
-    Usuario VARCHAR(50) NOT NULL UNIQUE,
-    Senha VARCHAR(255) NOT NULL,
-    Adm BIT NOT NULL,  -- 0 para nao, 1 para sim
-    CONSTRAINT PK_Usuario PRIMARY KEY (ID_Usuario)
+CREATE TABLE Users (
+    ID_Users INT IDENTITY(1,1),
+    FullName VARCHAR(100) NOT NULL,
+    Username VARCHAR(50) NOT NULL UNIQUE,
+    Password VARBINARY(64) NOT NULL,
+    Adm BIT NOT NULL,
+    CONSTRAINT PK_Usuario PRIMARY KEY (ID_Users)
 );
 
-CREATE TABLE Cidade (
-    ID_Cidade INT IDENTITY(1,1),
-    Descricao VARCHAR(100) NOT NULL,
+INSERT INTO Users (FullName, Username, Password, Adm) 
+VALUES ('Admin User', 'admin', HASHBYTES('SHA2_256', 'adminpassword'), 1);
+
+CREATE TABLE City (
+    ID_City INT IDENTITY(1,1),
+    Description VARCHAR(100) NOT NULL,
     UF CHAR(2) NOT NULL,
-    CONSTRAINT PK_Cidade PRIMARY KEY (ID_Cidade)
+    CONSTRAINT PK_City PRIMARY KEY (ID_City)
 );
 
-CREATE TABLE Cliente (
-    ID_Cliente INT IDENTITY(1,1),
-    Nome VARCHAR(50) NOT NULL,
-    Sobrenome VARCHAR(50) NOT NULL,
+INSERT INTO City (Description, UF) 
+VALUES ('São Paulo', 'SP');
+
+CREATE TABLE Customer (
+    ID_Customer INT IDENTITY(1,1),
+    Name VARCHAR(50) NOT NULL,
+    Surname VARCHAR(50) NOT NULL,
     CPF CHAR(11) NOT NULL UNIQUE,
-    Telefone VARCHAR(15) NOT NULL,
-    Endereco VARCHAR(150) NOT NULL,
-    ID_Cidade INT NOT NULL,
-    CONSTRAINT PK_Cliente PRIMARY KEY (ID_Cliente),
-    CONSTRAINT FK_Cliente_Cidade FOREIGN KEY (ID_Cidade) REFERENCES Cidade(ID_Cidade)
+    Cellphone VARCHAR(15) NOT NULL,
+    Address VARCHAR(150) NOT NULL,
+    ID_City INT NOT NULL,
+    CONSTRAINT PK_Customer PRIMARY KEY (ID_Customer),
+    CONSTRAINT FK_Customer_Cidade FOREIGN KEY (ID_City) REFERENCES City(ID_City) ON DELETE CASCADE
 );
 
-CREATE TABLE Fornecedor (
-    ID_Fornecedor INT IDENTITY(1,1),
-    NomeRazaoSocial VARCHAR(100) NOT NULL,
+INSERT INTO Customer (Name, Surname, CPF, Cellphone, Address, ID_City) 
+VALUES ('João', 'Silva', '12345678901', '11987654321', 'Rua A, 123', 1);
+
+CREATE TABLE Supplier (
+    ID_Supplier INT IDENTITY(1,1),
+    CorporateName VARCHAR(100) NOT NULL,
     CNPJ CHAR(14) NOT NULL UNIQUE,
-    Endereco VARCHAR(150) NOT NULL,
-    Telefone VARCHAR(15) NOT NULL,
-    ID_Cidade INT NOT NULL,
-    CONSTRAINT PK_Fornecedor PRIMARY KEY (ID_Fornecedor),
-    CONSTRAINT FK_Fornecedor_Cidade FOREIGN KEY (ID_Cidade) REFERENCES Cidade(ID_Cidade)
+    Address VARCHAR(150) NOT NULL,
+    Telephone VARCHAR(15) NOT NULL,
+    Representative NVARCHAR(150) NULL,
+    ID_City INT NOT NULL,
+    CONSTRAINT PK_Supplier PRIMARY KEY (ID_Supplier),
+    CONSTRAINT FK_Supplier_City FOREIGN KEY (ID_City) REFERENCES City(ID_City) ON DELETE CASCADE
 );
 
--- Interessante em algum momento criar uma tabela MARCAS, para que possa ser feito relatorios depois, evitando que o usuario digite 2 nomes que seriam iguais, de forma diferente
-CREATE TABLE Produto (
-    ID_Produto INT IDENTITY(1,1),
-    CodigoMarca VARCHAR(50),
-    Cor VARCHAR(30),
-    Descricao VARCHAR(255) NOT NULL,
-    Tamanho VARCHAR(10) NOT NULL,
-    Genero VARCHAR(10) NOT NULL,
-    Preco FLOAT(30) NOT NULL,
-    CONSTRAINT PK_Produto PRIMARY KEY (ID_Produto)
+INSERT INTO Supplier (CorporateName, CNPJ, Address, Telephone, Representative, ID_City) 
+VALUES ('Fornecedor Exemplo LTDA', '12345678000195', 'Avenida B, 456', '11876543210', 'Hugo', 1);
+
+CREATE TABLE Brand (
+    BrandId INT PRIMARY KEY IDENTITY(1,1),
+    BrandName NVARCHAR(100) NOT NULL
 );
 
-CREATE TABLE FornecedorProduto (
-    ID_Fornecedor INT,
-    ID_Produto INT,
-    CONSTRAINT PK_FornecedorProduto PRIMARY KEY (ID_Fornecedor, ID_Produto),
-    CONSTRAINT FK_FornecedorProduto_Fornecedor FOREIGN KEY (ID_Fornecedor) REFERENCES Fornecedor(ID_Fornecedor),
-    CONSTRAINT FK_FornecedorProduto_Produto FOREIGN KEY (ID_Produto) REFERENCES Produto(ID_Produto)
+INSERT INTO Brand (BrandName) 
+VALUES ('Marca Exemplo');
+
+CREATE TABLE Color (
+    ColorId INT PRIMARY KEY IDENTITY(1,1),
+    ColorName NVARCHAR(50) NOT NULL
 );
 
-INSERT INTO Usuarios(NomeCompleto, Usuario, Senha, Adm)
-VALUES
-		('admin', 'admin', 'admin', 1),
-		('Guilherme Cella', 'GuiCella', '1234', 1),
-		('gui', 'gui', 'gui', 1)
+INSERT INTO Color (ColorName) 
+VALUES ('Vermelho');
 
-INSERT INTO Cidade (Descricao, UF)
-VALUES 
-		('Erechim', 'RS'),
-		('Porto Alegre', 'RS'),
-		('Sao Paulo', 'SP'),
-		('Rio de Janeiro', 'RJ'),
-		('Curitiba', 'PR'),
-		('Florianopolis', 'SC'),
-		('Belo Horizonte', 'MG'),
-		('Brasilia', 'DF'),
-		('Salvador', 'BA'),
-		('Fortaleza', 'CE');
+CREATE TABLE Size (
+    SizeId INT PRIMARY KEY IDENTITY(1,1),
+    SizeDescription NVARCHAR(3) NOT NULL
+);
 
-INSERT INTO Fornecedor (NomeRazaoSocial, CNPJ, Endereco, Telefone, ID_Cidade)
-VALUES
-		('Fornecedor ABC', '12345678000199', 'Rua das Flores, 100', '51999990001', 1),  -- Erechim
-		('Fornecedor XYZ', '98765432000188', 'Av. Central, 200', '51999990002', 2);     -- Porto Alegre
+INSERT INTO Size (SizeDescription) 
+VALUES ('M');
 
-INSERT INTO Cliente (Nome, Sobrenome, CPF, Telefone, Endereco, ID_Cidade)
-VALUES
-		('Joao', 'Silva', '12345678901', '51999991111', 'Rua Verde, 50', 3),    -- Sao Paulo
-		('Maria', 'Oliveira', '98765432100', '51999992222', 'Av. Paulista, 101', 4);  -- Rio de Janeiro
+CREATE TABLE Product (
+    ID_Product INT IDENTITY(1,1),
+    BrandCode VARCHAR(50),
+    BrandId INT,
+    ColorId INT,
+    Description VARCHAR(255) NOT NULL,
+    SizeId INT,
+    Gender VARCHAR(10) NOT NULL,
+    Price DECIMAL(10, 2) NOT NULL,
+    CONSTRAINT PK_Product PRIMARY KEY (ID_Product),
+    CONSTRAINT FK_Product_Brand FOREIGN KEY (BrandId) REFERENCES Brand(BrandId),
+    CONSTRAINT FK_Product_Color FOREIGN KEY (ColorId) REFERENCES Color(ColorId),
+    CONSTRAINT FK_Product_Size FOREIGN KEY (SizeId) REFERENCES Size(SizeId)
+);
 
-INSERT INTO Produto(CodigoMarca, Cor, Descricao, Tamanho, Genero, Preco)
-VALUES
-		(NULL, 'Azul', 'Camiseta Basica', 'M', 'Masculino',85.00),  -- Produto sem Codigo de Marca
-		('001', 'Preto', 'Calca Jeans', 'G', 'Feminino', 95.00),
-		('002', 'Vermelho', 'Vestido Longo', 'P', 'Feminino',96.00),
-		('003', 'Branco', 'Tenis Esportivo', '42', 'Masculino',85.52),
-		(NULL, 'Verde', 'Jaqueta de Couro', 'GG', 'Unissex',96.52);  -- Produto sem Codigo de Marca
+INSERT INTO Product (BrandCode, BrandId, ColorId, Description, SizeId, Gender, Price) 
+VALUES ('MEX123', 1, 1, 'Camisa Vermelha', 1, 'Masculino', 59.90);
+
+CREATE TABLE ProductSupplier (
+    ID_Supplier INT,
+    ID_Product INT,
+    CONSTRAINT PK_ProductSupplier PRIMARY KEY (ID_Supplier, ID_Product),
+    CONSTRAINT FK_ProductSuppler_Supplier FOREIGN KEY (ID_Supplier) REFERENCES Supplier(ID_Supplier),
+    CONSTRAINT FK_SupplierProduct_Product FOREIGN KEY (ID_Product) REFERENCES Product(ID_Product),
+    CONSTRAINT UQ_ProductSupplier UNIQUE (ID_Supplier, ID_Product)
+);
+
+INSERT INTO ProductSupplier (ID_Supplier, ID_Product) 
+VALUES (1, 1);
+
+CREATE TABLE Sell (
+    ID_Sell INT IDENTITY(1,1),
+    ID_Customer INT NOT NULL,
+    Sell_Document INT NOT NULL,
+    SellDate DATETIME NOT NULL CONSTRAINT DF_SellDate DEFAULT GETDATE(),
+    TotalPrice DECIMAL(10, 2) NOT NULL,
+    PaymentMethod VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_Sell PRIMARY KEY (ID_Sell),
+    CONSTRAINT FK_Sell_Customer FOREIGN KEY (ID_Customer) REFERENCES Customer(ID_Customer) ON DELETE CASCADE
+);
+
+INSERT INTO Sell (ID_Customer, Sell_Document, TotalPrice) 
+VALUES (1, 1, 59.90);
+
+CREATE TABLE ItemSell (
+    ID_ItemSell INT IDENTITY(1,1),
+    ID_Sell INT NOT NULL,
+    ID_Product INT NOT NULL,
+    Qty INT NOT NULL, 
+    PartialPrice DECIMAL(10, 2) NOT NULL,
+    CONSTRAINT FK_ITEMVENDAS PRIMARY KEY (ID_ItemSell),
+    CONSTRAINT FK_ITEMVENDAS_PRODUTOS FOREIGN KEY (ID_Product) REFERENCES Product(ID_Product) ON DELETE CASCADE,
+    CONSTRAINT FK_ITEMVENDAS_VENDAS FOREIGN KEY (ID_Sell) REFERENCES Sell(ID_Sell) ON DELETE CASCADE
+);
+
+INSERT INTO ItemSell (ID_Sell, ID_Product, Qty, PartialPrice) 
+VALUES (1, 1, 1, 59.90);
+
+CREATE TABLE StockMovement (
+    ID_StockMovement INT IDENTITY(1,1) PRIMARY KEY,
+    ID_Product INT NOT NULL,
+    MDescription NVARCHAR(100) NOT NULL, 
+    Document INT NULL,
+    MovementType NVARCHAR(10) NOT NULL, CHECK (MovementType IN ('Entrada', 'Saida')),
+    Operation NVARCHAR(2) NOT NULL, CHECK (Operation IN ('E', 'S', 'I', 'R', 'D', 'T')),
+    Qty INT NOT NULL,
+    MovementDate DATETIME NOT NULL DEFAULT GETDATE(),
+    ID_Users INT NULL,
+    CONSTRAINT FK_Movement_Product FOREIGN KEY (ID_Product) REFERENCES Product(ID_Product) ON DELETE CASCADE,
+    CONSTRAINT FK_Movement_User FOREIGN KEY (ID_Users) REFERENCES Users(ID_Users) ON DELETE CASCADE
+);
+
+INSERT INTO StockMovement (ID_Product, MDescription, Document, MovementType, Operation, Qty, ID_Users) 
+VALUES (1, 'Movimento 1', 1, 'Entrada', 'E', 100, 1);
+
+CREATE TABLE Stock (
+    ID_Stock INT IDENTITY(1,1) PRIMARY KEY,
+    ID_Product INT NOT NULL,
+    Qty INT NOT NULL,
+    CONSTRAINT FK_Stock_Product FOREIGN KEY (ID_Product) REFERENCES Product(ID_Product) ON DELETE CASCADE
+);
+
+INSERT INTO Stock (ID_Product, Qty) 
+VALUES (1, 100);
