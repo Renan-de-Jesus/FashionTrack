deb7886af579b03a19816c87728a695aab7d0914
Renan de Jesus <renan_dejesus24@hotmail.com>
2025-01-31 01:03:35 -0300

    Removal of the customer ID on the sales view screen and addition of a button, on the same screen, that leads to making a new sale

--- FashionTrack/SellList.xaml.cs
+++ FashionTrack/SellList.xaml.cs
@@ -28,8 +28,7 @@ namespace FashionTrack
                     conn.Open();
                     string query = @"
                         SELECT 
-                            s.ID_Sell, 
-                            s.ID_Customer, 
+                            s.ID_Sell,  
                             s.Sell_Document, 
                             s.SellDate, 
                             s.TotalPrice, 
@@ -60,50 +59,9 @@ namespace FashionTrack
         }
 
 
-
-        private void DeleteSellButton_Click(object sender, RoutedEventArgs e)
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
         {
-            if (SellDataGrid.SelectedItem is DataRowView selectedRow)
-            {
-                int sellId = Convert.ToInt32(selectedRow["ID_Sell"]);
-
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-                    string query = "DELETE FROM Sell WHERE ID_Sell = @ID_Sell";
-                    using (SqlCommand cmd = new SqlCommand(query, conn))
-                    {
-                        cmd.Parameters.AddWithValue("@ID_Sell", sellId);
-                        cmd.ExecuteNonQuery();
-                    }
-                }
-
-                MessageBox.Show("Venda deletada com sucesso.", "Sucesso", MessageBoxButton.OK, MessageBoxImage.Information);
-                LoadSell();
-            }
-            else
-            {
-                MessageBox.Show("Por favor selecione uma venda para deletar.", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
-        private void NewSellButton_Click(object sender, RoutedEventArgs e)
-        {
-            SellScreen sellScreen = new SellScreen();
-            sellScreen.Closed += (s, args) => LoadSell();
-            sellScreen.ShowDialog();
-        }
-
-        private void SellDataGrid_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            if (SellDataGrid.SelectedItem is DataRowView selectedRow)
-            {
-                int sellId = Convert.ToInt32(selectedRow["ID_Sell"]);
-
-                SellScreen sellRegister = new SellScreen(sellId);
-                sellRegister.Closed += (s, args) => LoadSell();
-                sellRegister.ShowDialog();
-            }
+            this.Close();
         }
     }
 }
